d4c1b5111ebd898058a6d3becebdc248b3f12929
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-02-17 21:49:34 +0200

    Edit profile bloc

--- lib/controllers/profile_bloc/profile_event.dart
+++ lib/controllers/profile_bloc/profile_event.dart
@@ -10,12 +10,15 @@ class UpdateProfileEvent extends ProfileEvent {
   UpdateProfileEvent(this.user);
 }
 
-class UpdateProfileImageFromCamEvent extends ProfileEvent{}
-class UpdateProfileImageFromGallEvent extends ProfileEvent{}
+// class UpdateProfileImageFromCamEvent extends ProfileEvent{}
+// class UpdateProfileImageFromGallEvent extends ProfileEvent{}
 class RemoveProfileImageEvent extends ProfileEvent{}
 
 class ToggleEditingEvent extends ProfileEvent {}
 
 class TogglePasswordVisibilityEvent extends ProfileEvent {}
 
-class UpdateAvatarEvent extends ProfileEvent {}
+class UpdateAvatarEvent extends ProfileEvent {
+  final bool isFromCamera;
+  UpdateAvatarEvent(this.isFromCamera);
+}
