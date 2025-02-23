32f8c8100cdd318156708c1e78bc9470b6c4cd8b
Noura Tarek <99992263+noura2tarek@users.noreply.github.com>
2025-02-23 21:18:26 +0200

    Add fingerprint auth to profile

--- lib/views/screens/home/widgets/app_bottom_navigation_bar.dart
+++ lib/views/screens/home/widgets/app_bottom_navigation_bar.dart
@@ -1,22 +1,118 @@
+import 'dart:developer';
 import 'package:easy_localization/easy_localization.dart';
 import 'package:egypt_tourist_guide/controllers/places_bloc/places_bloc.dart';
 import 'package:egypt_tourist_guide/core/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:local_auth/local_auth.dart';
 
-
-class AppBottomNavigationBar extends StatelessWidget {
+class AppBottomNavigationBar extends StatefulWidget {
   const AppBottomNavigationBar({super.key});
+
+  @override
+  State<AppBottomNavigationBar> createState() => _AppBottomNavigationBarState();
+}
+
+class _AppBottomNavigationBarState extends State<AppBottomNavigationBar> {
+  late final LocalAuthentication auth;
+  bool _isAuthenticated = false;
+  bool _isAvailable = false;
+
+  // Check if the device supports biometrics or not
+  _checkAuthFeature() async {
+    try {
+      final bool canAuthenticateWithBiometrics = await auth.canCheckBiometrics;
+      final bool canAuthenticate =
+          canAuthenticateWithBiometrics || await auth.isDeviceSupported();
+      if (canAuthenticate || canAuthenticateWithBiometrics) {
+        _isAvailable = true;
+        setState(() {});
+      } else {
+        log('Auth with biometrics feature is not available');
+        _isAvailable = false;
+        setState(() {});
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('auth-not-available'.tr()),
+        ));
+      }
+    } on PlatformException catch (e) {
+      _isAvailable = false;
+      setState(() {});
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('auth-not-available'.tr()),
+      ));
+      log('Error occurred(can not authenticate): $e');
+    }
+  }
+
+  // Auth with biometrics fingerprint method
+  authWithBiometrics() async {
+    try {
+      if (!_isAvailable) {
+        // Assuming the user is authenticated
+        setState(() {
+          _isAuthenticated = true;
+        });
+        return;
+      }
+      // auth using fingerprint biometrics
+      final bool authenticated = await auth.authenticate(
+        localizedReason: 'scan-fingerprint'.tr(),
+        options: const AuthenticationOptions(
+          biometricOnly: true,
+          stickyAuth: true,
+        ),
+      );
+      setState(() {
+        _isAuthenticated = authenticated;
+      });
+      if (_isAuthenticated) {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('authentication-Done'.tr()),
+        ));
+      }
+    } catch (e) {
+      // That means the user device does not support biometrics
+      // Assuming the user is authenticated
+      setState(() {
+        _isAuthenticated = true;
+      });
+      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+        content: Text('auth-not-available'.tr()),
+      ));
+    }
+  }
+
+  @override
+  void initState() {
+    // initialize local authentication
+    auth = LocalAuthentication();
+    _checkAuthFeature();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     return BlocBuilder<PlacesBloc, PlacesState>(
       builder: (context, state) {
-        final placesBloc = context.read<PlacesBloc>();
+        final placesBloc = PlacesBloc.get(context);
         return BottomNavigationBar(
           backgroundColor: AppColors.purple,
-          onTap: (int pageIndex) {
-            placesBloc.add(ChangeBottomNavigationIndexEvent(pageIndex));
+          onTap: (int pageIndex) async {
+            if (pageIndex == 3) {
+              // Check finger print auth to access profile screen
+              await authWithBiometrics();
+              if (_isAuthenticated) {
+                placesBloc.add(ChangeBottomNavigationIndexEvent(pageIndex));
+              } else if (!_isAuthenticated) {
+                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                  content: Text('access-denied'.tr()),
+                ));
+              }
+            } else {
+              placesBloc.add(ChangeBottomNavigationIndexEvent(pageIndex));
+            }
           },
           currentIndex: placesBloc.currentPageIndex,
           selectedItemColor: AppColors.purple,
